a81d3aff064fee7dbd856029cb6ae817b200103e
geferson <gefersonlj@gmail.com>
2023-06-21 20:41:42 -0300

    Alteração do Activity_main para tela de login

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.helloandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 // Comentário de alteração 1.1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btLogin).setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener {
+            startActivity(Intent(this, EsqueciSenhaActivity::class.java))
+        }
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener {
+            startActivity(Intent(this, CadastroActivity::class.java))
+        }
     }
 }
