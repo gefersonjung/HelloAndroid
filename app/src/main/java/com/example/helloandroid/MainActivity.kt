1b858afae7a3f2371260a92fa833730b3367025a
geferson <gefersonlj@gmail.com>
2023-06-21 23:38:15 -0300

    criação de novas activities e tratamento dos botões de chamada das activities

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -12,14 +12,66 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btLogin).setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+        //organizando o código com uma função separada para cada chamada
+        findViewById<Button>(R.id.btLogin).setOnClickListener{
+            //delega o tratamento para o método correto
+            onClickLogin()
         }
-        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener {
-            startActivity(Intent(this, EsqueciSenhaActivity::class.java))
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener{
+            onClickEsqueciSenha()
         }
-        findViewById<TextView>(R.id.btCadastrar).setOnClickListener {
-            startActivity(Intent(this, CadastroActivity::class.java))
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener{
+            onClickCadastrar()
         }
+
+
+    }
+    private fun onClickLogin(){
+        startActivity(Intent(this,HomeActivity::class.java))
+    }
+    private fun onClickEsqueciSenha(){
+        startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+    }
+    private fun onClickCadastrar(){
+        startActivity(Intent(this,CadastroActivity::class.java))
     }
 }
+
+       /* //utilização da classe amonima para chamar a função
+        findViewById<Button>(R.id.btLogin).setOnClickListener(object: View.OnClickListener{
+            override fun onClick(view: View?){
+                startActivity(Intent(this@MainActivity,HomeActivity::class.java))
+            }
+        })
+        //utilização do lambda para chamar a função
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener({
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        })
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?){
+        when(view?.id){
+          /*  R.id.btLogin -> {
+                startActivity(Intent(this,HomeActivity::class.java))
+            }
+            R.id.btEsqueciSenha -> {
+                startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+            } */
+            R.id.btCadastrar -> {
+                startActivity(Intent(this,CadastroActivity::class.java))
+            }
+        }
+    }
+
+//        findViewById<Button>(R.id.btLogin).setOnClickListener {
+//            startActivity(Intent(this, HomeActivity::class.java))
+//        }
+//        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener {
+//            startActivity(Intent(this, EsqueciSenhaActivity::class.java))
+//        }
+//        findViewById<TextView>(R.id.btCadastrar).setOnClickListener {
+//            startActivity(Intent(this, CadastroActivity::class.java))
+//        }
+
+}  */
